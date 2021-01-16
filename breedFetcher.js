ae0596739c50e03eb71acd530ae96777b34317b4
JJ <jerrica.mj@gmail.com>
2021-01-16 23:31:07 +0000

    Added .gitignore; installed npm and request and have started the breedFetcher.js with requiring request. -jj

---
+++ breedFetcher.js
@@ -0,0 +1,6 @@
+// CATS AS A SERVICE ASSIGNMENT
+
+// Build a comand line app that makes it "easy" for users to query TheCatAPI (an HTTP API that provides JSON data about Cats) dataset from the terminal.
+// Users can provide any breed name, causing our app to fetch the info from the API and pring out a short description of that breed
+
+const request = require("request");
